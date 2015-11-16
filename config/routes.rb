75ba43d9e2c69921ed9958489e8b90b1bc61b43a
fedearribas <fede.arribas@gmail.com>
2015-11-16 23:02:22 +0000

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
 
-  get 'static_pages/help'
-  
-  get 'static_pages/about'
+  get 'users/new'
+
+  get 'help' => 'static_pages#help'
+  get 'about'=> 'static_pages#about'
+  get 'contact'=>'static_pages#contact'
+  get 'signup' => 'users#new'
   
   root 'static_pages#home'
 
