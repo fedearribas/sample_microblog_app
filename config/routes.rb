a45c390b1d92f55a38c5120d3b98e7e369225e82
fedearribas <fede.arribas@gmail.com>
2015-11-27 15:04:19 +0000

    Finish log in / log out

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,17 @@ Rails.application.routes.draw do
 
 resources :users
 
+  get 'sessions/new'
   get 'users/new'
 
   get 'help' => 'static_pages#help'
   get 'about'=> 'static_pages#about'
   get 'contact'=>'static_pages#contact'
   get 'signup' => 'users#new'
+  
+  get 'login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
   
   root 'static_pages#home'
 
